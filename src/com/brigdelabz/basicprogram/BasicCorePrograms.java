1a57cacc16567fcb0f51352919ea8c512755405e
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-19 08:25:23 +0530

    Added: code flip coin heads or tails

--- src/com/brigdelabz/basicprogram/BasicCorePrograms.java
+++ src/com/brigdelabz/basicprogram/BasicCorePrograms.java
@@ -1,7 +1,22 @@
 package com.brigdelabz.basicprogram;
 
+import java.util.Scanner;
+
 public class BasicCorePrograms {
+    public static void coinFlip() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of times to flip coin");
+        int num = scan.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if (Math.random() < 0.5) {
+                System.out.println("Tails");
+            } else {
+                System.out.println("Heads");
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("core java programs");
+        coinFlip();
     }
 }
