03a0acb73e6df5bc2300682a91b0306f88e4705c
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-19 17:30:49 +0530

    Added:code prime factor

--- src/com/brigdelabz/basicprogram/BasicCorePrograms.java
+++ src/com/brigdelabz/basicprogram/BasicCorePrograms.java
@@ -48,11 +48,30 @@ package com.brigdelabz.basicprogram;
         }
         System.out.println("nth harmonic value is:="+sum);
     }
-    public static void main(String[] args) {
+       public static void primeFactors()
+       {
+          int n=90;
+           while (n%2==0)
+           {
+               System.out.print(2 + " ");
+               n /= 2;
+           }
+           for (int i = 3; i <= Math.sqrt(n); i+= 2) {
+               while (n % i == 0) {
+                   System.out.print(i + " ");
+                   n /= i;
+               }
+           }
+           if (n > 2)
+               System.out.print(n);
+       }
+
+       public static void main(String[] args) {
         System.out.println("core java programs");
         coinFlip();
         leapYear();
         int num=Integer.parseInt(args[0]);
         printTable(num);
+        primeFactors();
     }
 }
