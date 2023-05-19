2c77877f8dc4d8a28e355a0bd8daa1b8e12a7f13
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-19 17:39:59 +0530

    Added:code calculate quotient remainder

--- src/com/brigdelabz/basicprogram/BasicCorePrograms.java
+++ src/com/brigdelabz/basicprogram/BasicCorePrograms.java
@@ -65,6 +65,17 @@ package com.brigdelabz.basicprogram;
            if (n > 2)
                System.out.print(n);
        }
+       public static void calculateQuotientRemainder(){
+       System.out.println("Enter the value of num1 and num2");
+       Scanner sc = new Scanner(System.in);
+       int num1 = sc.nextInt();
+       int num2 = sc.nextInt();
+       int quotient = 0, remainder = 0;
+       quotient = num1 / num2;
+       remainder = num1 % num2;
+       System.out.println("Quotient when " + num1 + "/" + num2 + " is: " + quotient);
+       System.out.println("Remainder when " + num1 + " is divided by " + num2 + " is: " + remainder);
+   }
 
        public static void main(String[] args) {
         System.out.println("core java programs");
@@ -73,5 +84,6 @@ package com.brigdelabz.basicprogram;
         int num=Integer.parseInt(args[0]);
         printTable(num);
         primeFactors();
+        calculateQuotientRemainder();
     }
 }
